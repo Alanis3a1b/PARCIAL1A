ee7c15a0f51da382fc235445a23b3b6ed33a7017
Daniela <Daniela@DANI>
2024-03-09 09:46:04 -0600

    Tablas actualizadas

--- PARCIAL1A/Models/librosContext.cs
+++ PARCIAL1A/Models/librosContext.cs
@@ -1,6 +1,22 @@
-﻿namespace PARCIAL1A.Models
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace PARCIAL1A.Models
 {
-    public class librosContext
+
+    public class librosContext : DbContext
     {
+        public librosContext(DbContextOptions<librosContext> options) : base(options)
+        {
+        }
+        public DbSet<libros> libros { get; set; }
+
+        public DbSet<autores> autores { get; set; }
+
+        public DbSet<posts> posts { get; set; }
+        public DbSet<autor_libro> estados_equipo { get; set; }
+        /*Tablas*/
+
+
     }
 }
