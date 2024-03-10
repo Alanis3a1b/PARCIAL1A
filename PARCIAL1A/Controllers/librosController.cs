92e069d7be589ea60f423457836b11213e386b3c
Alanis3a1b <alanis.alvarez@catolica.edu.sv>
2024-03-09 19:09:26 -0600

    Solucion

--- PARCIAL1A/Controllers/librosController.cs
+++ PARCIAL1A/Controllers/librosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PARCIAL1A.Models;
 
 namespace PARCIAL1A.Controllers
@@ -14,5 +15,83 @@ namespace PARCIAL1A.Controllers
         {
             _librosContext = librosContext;
         }
+
+        //Método para leer todos los registros
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<Libros> listadolibros = (from e in _librosContext.libros
+                                            select e).ToList();
+
+            if (listadolibros.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadolibros);
+        }
+
+        //Método para buscar por ID
+        [HttpGet]
+        [Route("GetById/{id}")]
+
+        public IActionResult Get(int id)
+        {
+            Libros libro = (from e in _librosContext.libros
+                             where e.id_libros == id
+                             select e).FirstOrDefault();
+
+            if (libro == null)
+            {
+                return NotFound();
+
+            }
+
+            return Ok(libro);
+        }
+
+
+        //Método para crear o insertar registros
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarLibros([FromBody] Libros libro)
+        {
+            try
+            {
+                _librosContext.libros.Add(libro);
+                _librosContext.SaveChanges();
+                return Ok(libro);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //Método para modificar los registros de la tabla
+        //[HttpPut]
+        //[Route("Actualizar/{id}")]
+
+        //public IActionResult ActualizarLibros(int id, [FromBody] Libros libroModificar)
+        //{
+        //    Libros libroActual = (from e in _librosContext.Libros
+        //                           where e.id_libros == id
+        //                           select e).FirstOrDefault();
+
+        //    if (libroActual == null)
+        //    { return NotFound(); }
+
+        //    libroActual.nombre_autores = libroModificar.nombre_autores;
+
+
+        //    _autoresContexto.Entry(autorActual).State = EntityState.Modified;
+        //    _autoresContexto.SaveChanges();
+
+        //    return Ok(autorModificar);
+
+        //}
     }
 }
